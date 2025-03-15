688dcb072f317b0df718656d3987240bcc9f8057
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2025-03-15 07:39:25 +0500

    core gunicorn

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 
 # Start Django server
 echo "Starting Django server"
-gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 4
+gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 4
